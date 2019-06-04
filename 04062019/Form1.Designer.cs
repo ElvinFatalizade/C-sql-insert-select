4d894c735d6e3028a2cc5d33d8033dd37da7afe1
Elvin Fatalizade <elvinff@code.edu.az>
2019-06-04 15:41:48 +0400

    C#SQL

---
+++ 04062019/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _04062019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumericOrder = new System.Windows.Forms.NumericUpDown();
+            this.DvgCatigoreis = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericOrder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DvgCatigoreis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn
+            // 
+            this.Btn.Location = new System.Drawing.Point(21, 264);
+            this.Btn.Name = "Btn";
+            this.Btn.Size = new System.Drawing.Size(159, 51);
+            this.Btn.TabIndex = 0;
+            this.Btn.Text = "Add";
+            this.Btn.UseVisualStyleBackColor = true;
+            this.Btn.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.NumericOrder);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TxtName);
+            this.groupBox1.Controls.Add(this.Btn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 353);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Katiqoriya";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(50, 74);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(208, 22);
+            this.TxtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Order Number";
+            // 
+            // NumericOrder
+            // 
+            this.NumericOrder.Location = new System.Drawing.Point(50, 182);
+            this.NumericOrder.Name = "NumericOrder";
+            this.NumericOrder.Size = new System.Drawing.Size(120, 22);
+            this.NumericOrder.TabIndex = 4;
+            // 
+            // DvgCatigoreis
+            // 
+            this.DvgCatigoreis.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DvgCatigoreis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DvgCatigoreis.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.DvgCatigoreis.Location = new System.Drawing.Point(374, 22);
+            this.DvgCatigoreis.Name = "DvgCatigoreis";
+            this.DvgCatigoreis.RowTemplate.Height = 24;
+            this.DvgCatigoreis.Size = new System.Drawing.Size(564, 347);
+            this.DvgCatigoreis.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 574);
+            this.Controls.Add(this.DvgCatigoreis);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericOrder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DvgCatigoreis)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown NumericOrder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.DataGridView DvgCatigoreis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ 04062019/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _04062019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FillCatigories();
+
+
+        }
+        private void Btn_Click(string name)
+        {
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            con.Open();
+            string query = "INSERT INTO FACULTE([Name]) VALUES(@name)";
+            SqlCommand command = new SqlCommand(query, con);
+
+            command.Parameters.AddWithValue("@name", name);
+          
+
+            int rowAffercted= command.ExecuteNonQuery();
+
+            con.Close();
+
+            FillCatigories();
+
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Btn_Click(TxtName.Text);
+        }
+
+
+        private void FillCatigories()
+
+        {
+            DvgCatigoreis.Rows.Clear();
+
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            con.Open();
+
+            string query = "SELECT * FROM  FACULTE  ORDER BY id ASC ";
+
+            SqlCommand command = new SqlCommand(query, con);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                //MessageBox.Show(reader.GetString(1));
+
+                DvgCatigoreis.Rows.Add(reader.GetInt32(0), reader.GetString(1));
+            }
+
+            con.Close();
+        }
+    }
+}
